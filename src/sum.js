c6a57036231d33bde33cfa0885a462f44745ee67
vikas <vikkycorner@gmail.com>
2018-01-17 17:41:02 +0530

    refactored sum function

--- src/sum.js
+++ src/sum.js
@@ -1,31 +1,28 @@
 const sum = function(array, values) {
-  if(values) {
-    if(!Array.isArray(values)) {
-      const firstValue = array[0];
-      const updatedArray = array.slice(1);
-      if(!firstValue[values]) {
-        return 0;
-      }
-      return updatedArray.reduce((acc, currentObject) => {
-        return acc + currentObject[values];
-      }, firstValue[values])
-    }
-    return values.map((value) => {
-      const firstValue = array[0];
-      const updatedArray = array.slice(1);
-      if(!firstValue[value]) {
-        return 0;
-      }
-      return updatedArray.reduce((acc, currentObject) => {
-        return acc + currentObject[value];
-      }, firstValue[value])
-    })
-  }
   const firstValue = array[0];
   const updatedArray = array.slice(1);
+
+  if(values) {
+    return (Array.isArray(values))
+      ? calculatedSumOfValues(firstValue, updatedArray, values )
+      : calculateValueSum(firstValue, updatedArray, values)
+  }
   return updatedArray.reduce((acc, value) => {
     return acc + value;
   }, firstValue)
+}
+
+const calculatedSumOfValues = function(firstValue, array, values ) {
+  return values.map((value) => {
+    return calculateValueSum(firstValue, array, value);
+  })
+}
+
+const calculateValueSum = function(firstValue, array, value) {
+  return ( !firstValue[value] ) ? 0 
+    : array.reduce((acc, currentObject) => {
+      return acc + currentObject[value];
+    }, firstValue[value]);
 }
 
 module.exports = sum;
