967fcb916fd2031dadb1e7f74cee4f9257b2411b
vikas <vikkycorner@gmail.com>
2018-02-23 00:58:44 +0530

    added function zipWith

--- index.js
+++ index.js
@@ -21,3 +21,4 @@ exports.pull = require('./src/pull');
 exports.reverse = require('./src/reverse');
 exports.toPairs = require('./src/toPairs');
 exports.invertObj = require('./src/invertObj');
+exports.zipWith = require('./src/zipWith');
