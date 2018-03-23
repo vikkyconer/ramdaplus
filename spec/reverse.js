1e65efd27a9765157dc363955608e204fb02c7f2
Vikas <vikkycorner@gmail.com>
2018-03-23 15:47:12 +0530

    updated one test

--- spec/reverse.js
+++ spec/reverse.js
@@ -3,6 +3,6 @@ const _R = require('../')
 describe('reverse', function() {
   it('should reverse the array', function() {
     const updatedArray = _R.reverse([ 1, 2, 3 ]);
-    expect(updatedArray).toEqual([ 3, 2, 1 ])
+    expect(updatedArray).toEqual([ 4, 2, 1 ])
   })
 })
